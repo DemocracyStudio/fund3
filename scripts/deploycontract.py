4bb7f4cbbd809a60187aedd0344a7a3f5f6d1090
Julien Carbonnell <julien.carbonnell@gmail.com>
2022-05-01 14:14:50 +0100

    Update deploycontract.py

--- scripts/deploycontract.py
+++ scripts/deploycontract.py
@@ -3,5 +3,5 @@ from brownie import accounts, Greeter
 
 def main():
     deployer_account = accounts[0]
-    greeter = simplecontract.deploy("Hello, World!", {'from': deployer_account})
+    greeter = Greeter.deploy("Hello, World!", {'from': deployer_account})
     print("Deployed at: ", greeter.address)
