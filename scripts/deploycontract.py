eccd44a752f579a94a8a1fb100f8b5032f4e047e
Julien Carbonnell <julien.carbonnell@gmail.com>
2022-05-01 07:54:51 +0100

    Add files via upload

---
+++ scripts/deploycontract.py
@@ -0,0 +1,7 @@
+# create a the deployment script in deploy.py file
+from brownie import accounts, Greeter
+
+def main():
+    deployer_account = accounts[0]
+    greeter = simplecontract.deploy("Hello, World!", {'from': deployer_account})
+    print("Deployed at: ", greeter.address)
