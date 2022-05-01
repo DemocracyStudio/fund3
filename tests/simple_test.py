08e5413527a93166594d641267e7d7629be71e61
Julien Carbonnell <julien.carbonnell@gmail.com>
2022-05-01 14:21:02 +0100

    Update simple_test.py

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -3,12 +3,14 @@ import pytest
 from brownie import Greeter, accounts
 
 @pytest.fixture
-def simple():
-    return accounts[0].deploy(simplecontract, "Hello, World!")
+def greeter():
+    return accounts[0].deploy(Greeter, "Hello, World!")
+
     # Check if the contract was deployed with the correct greeting
-    def simple_test(simple):
-        assert simple.greet() == "Hello, World!"
+    def simple_test(greeter):
+        assert greeter.greet() == "Hello, World!"
+        
     # Check if the greeting changes
-    def test_set_simple(simple):
-        simple.setGreeting("Hola, mundo!")
-        assert simple.greet() == "Hola, mundo!"
+    def test_set_simple(greeter):
+        greeter.setGreeting("Hola, mundo!")
+        assert greeter.greet() == "Hola, mundo!"
