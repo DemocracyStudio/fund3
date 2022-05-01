ad31b6d0a43808f6d5a03d128d497b07a1979f96
Julien Carbonnell <julien.carbonnell@gmail.com>
2022-05-01 07:53:48 +0100

    Add files via upload

---
+++ contracts/simplecontract.sol
@@ -0,0 +1,18 @@
+# create smart contract simplecontract.sol file
+pragma solidity ^0.8.0;
+
+contract simplecontract {
+  string greeting;
+
+  constructor(string memory _greeting) {
+    greeting = _greeting;
+  }
+
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+
+  function setGreeting(string memory _greeting) public {
+    greeting = _greeting;
+  }
+}
